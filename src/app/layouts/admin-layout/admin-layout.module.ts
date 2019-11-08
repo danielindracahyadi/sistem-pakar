8a551d148244befeb849a9d89a9cdff247372146
Cindy Michelle <cindy@kodefox.com>
2019-11-08 21:24:47 +0700

    Add Main Page, define font family

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AdminLayoutRoutes } from './admin-layout.routing';
-import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
+import {MainComponent} from './../../pages/main/main.component';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AdminLayoutRoutes} from './admin-layout.routing';
+import {DashboardComponent} from '../../dashboard/dashboard.component';
+import {UserProfileComponent} from '../../user-profile/user-profile.component';
+import {TableListComponent} from '../../table-list/table-list.component';
+import {TypographyComponent} from '../../typography/typography.component';
+import {IconsComponent} from '../../icons/icons.component';
+import {MapsComponent} from '../../maps/maps.component';
+import {NotificationsComponent} from '../../notifications/notifications.component';
+import {UpgradeComponent} from '../../upgrade/upgrade.component';
 
 import {
   MatButtonModule,
@@ -18,7 +19,7 @@ import {
   MatRippleModule,
   MatFormFieldModule,
   MatTooltipModule,
-  MatSelectModule
+  MatSelectModule,
 } from '@angular/material';
 @NgModule({
   imports: [
@@ -42,7 +43,7 @@ import {
     MapsComponent,
     NotificationsComponent,
     UpgradeComponent,
-  ]
+    MainComponent,
+  ],
 })
-
 export class AdminLayoutModule {}
