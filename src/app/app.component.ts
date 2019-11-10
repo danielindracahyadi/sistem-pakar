4fdb7e4dee842477b2669aef7b6b430fad237daa
danielindracahyadi <danielindrac@gmail.com>
2019-11-11 00:08:58 +0700

    Update init Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
-import { Component} from '@angular/core';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  main: boolean = true;
+  firstTree: boolean = false;
+  problems = [
+    {
+      nama: 'layar mati',
+    },
+    {
+      nama: 'komputer mati',
+    },
+    {
+      nama: 'mati lampu',
+    },
+  ];
 
+  start() {
+    this.main = false;
+    this.firstTree = true;
+  }
 }
