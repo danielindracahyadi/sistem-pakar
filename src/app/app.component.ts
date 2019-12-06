f4e2d3fbe9bbda86b9613a9924a02eb93870f2a2
Cindy Michelle <cindy@kodefox.com>
2019-12-07 02:28:08 +0700

    add more more layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   main: boolean = true;
   firstTree: boolean = false;
+  result: boolean = false;
+
+  history = [];
   problems = [
     {
       nama: 'layar mati',
@@ -20,8 +23,36 @@ export class AppComponent {
     },
   ];
 
+  selectedOption = null;
+
+  onOptionPress(idx: number) {
+    this.selectedOption = this.problems[idx].nama;
+  }
+
   start() {
     this.main = false;
     this.firstTree = true;
+  }
+
+  back() {
+    // TODO : check if current is the first question, it should go back to main screen
+    // else go back to prev question
+    const back = 1; // dummy
+    if (back) {
+      this.main = true;
+      this.firstTree = false;
+    } else {
+    }
+  }
+
+  next() {
+    // TODO : check if current is the last question, it should navigate to result screen
+    // else navigate to next question
+
+    // pilih jawaban dulu baru bisa next
+    if (this.selectedOption != null) {
+      this.result = true;
+      this.firstTree = false;
+    }
   }
 }
